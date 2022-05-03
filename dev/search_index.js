29a013ca703df6e48c1d2ccec5a0f10f5c8b73d9
Documenter.jl <documenter@juliadocs.github.io>
2022-05-03 15:23:14 +0000

    build based on 35d4886

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#juliaci.jl","page":"juliaci.jl","title":"juliaci.jl","text":"","category":"section"},{"location":"","page":"juliaci.jl","title":"juliaci.jl","text":"Documentation for juliaci.jl","category":"page"}]
+}
